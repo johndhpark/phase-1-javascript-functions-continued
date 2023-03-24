7d2e08524a0ad887f03aa3802028adb6f5492d6e
John DH. Park <john.dh.park@me.com>
2023-03-24 12:39:11 -0700

    completed functions lab continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity) {
+	activity = activity == undefined ? "roller-skate" : activity;
+
+	return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity) {
+	activity = activity == undefined ? "go to the office" : activity;
+
+	return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapper) {
+	if (wrapper === "*") {
+		return (str) => `You are *${str}*!`;
+	} else if (wrapper === "||") {
+		return (str) => `You are ||${str}||!`;
+	} else {
+		return (str) => `You are ${str}!`;
+	}
+}
